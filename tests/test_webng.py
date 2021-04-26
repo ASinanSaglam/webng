dca7034a116ce4e8e597e82e72e1ef5354d87d44
Ali Sinan Saglam <asinansaglam@gmail.com>
2021-04-26 15:16:32 -0400

    very basic working tests off of `make test`

--- tests/test_webng.py
+++ tests/test_webng.py
@@ -1,6 +1,9 @@
 
+import os, shutil
 from pytest import raises
 from webng.main import weBNGTest
+
+tfold = os.path.dirname(__file__)
 
 def test_webng():
     # test webng without any subcommands or arguments
@@ -17,20 +20,70 @@ def test_webng_debug():
         assert app.debug is True
 
 
-def test_command1():
-    # test command1 without arguments
-    argv = ['command1']
+# Template test
+def test_template():
+    # test template without arguments
+    os.chdir(tfold)
+    with raises(SystemExit):
+        argv = ['template', '-h']
+        with weBNGTest(argv=argv) as app:
+            app.run()
+            assert app.exit_code == 0
+
+    # test template with arguments
+    os.chdir(tfold)
+    fpath = os.path.join(tfold, "test.bngl")
+    opath = os.path.join(tfold, "test_setup.yaml")
+    argv = ['template', '-i', '{}'.format(fpath), '-o', '{}'.format(opath)]
     with weBNGTest(argv=argv) as app:
         app.run()
-        data,output = app.last_rendered
-        assert data['foo'] == 'bar'
-        assert output.find('Foo => bar')
+        assert os.path.isfile(opath)
 
+
+# Setup test
+def test_setup():
+    # test command1 without arguments
+    argv = ['setup']
+    with raises(SystemExit):
+        with weBNGTest(argv=argv) as app:
+            app.run()
+            assert app.exit_code == 0
 
     # test command1 with arguments
-    argv = ['command1', '--foo', 'not-bar']
+    os.chdir(tfold)
+    fpath = os.path.join(tfold, "test_setup.yaml")
+    opath = os.path.join(tfold, "test")
+    argv = ['setup', '--opts', '{}'.format(fpath)]
     with weBNGTest(argv=argv) as app:
         app.run()
-        data,output = app.last_rendered
-        assert data['foo'] == 'not-bar'
-        assert output.find('Foo => not-bar')
+        assert os.path.isdir(opath)
+    # clean up
+    os.remove(fpath)
+    shutil.rmtree(opath)
+
+
+# TODO: Write tests for each analysis 
+
+# Average test  
+# def test_avg_analysis():
+#     # test command1 without arguments
+#     argv = ['analysis']
+#     with weBNGTest(argv=argv) as app:
+#         app.run()
+
+#     # test command1 with arguments
+#     argv = ['analysis --opts {}'.format("test_average.yaml")]
+#     with weBNGTest(argv=argv) as app:
+#         app.run()
+
+# Evolution test
+# def test_evo_analysis():
+#     # test command1 without arguments
+#     argv = ['analysis']
+#     with weBNGTest(argv=argv) as app:
+#         app.run()
+
+#     # test command1 with arguments
+#     argv = ['analysis --opts {}'.format("test_evolution.yaml")]
+#     with weBNGTest(argv=argv) as app:
+#         app.run()
